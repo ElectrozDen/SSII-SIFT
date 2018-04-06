dc9746f03c263ce253bf429ecce328210eeb9bf4
Corentin Artaud <corentin.artaud@etu.unice.fr>
2018-04-06 21:11:47 +0200

    create and feed the structure

--- app.ts
+++ app.ts
@@ -1,3 +1,37 @@
-const cv = require('opencv4nodejs');
+import * as cv from 'opencv4nodejs';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as kmeans from 'ml-kmeans'
 
-console.log('test ma gueule');
+class ImageDescription {
+    name : string;
+    image : cv.Mat;
+    label : number;
+    constructor(name: string, image: cv.Mat){
+        this.name = name;
+        this.image = image;
+        this.label = -1;
+    }
+}
+
+const dirPath : string = path.resolve('./images');
+const files : string [] = fs.readdirSync(dirPath);
+
+const images : ImageDescription[] = files
+    .map(file => path.resolve(dirPath,file))
+    .map(filepath => cv.imread(filepath))
+    .map(img => img.bgrToGray())
+    .map((realImg, i) => new ImageDescription(files[i], realImg));
+
+
+
+const isImageFour = (_, i : number) : boolean => files[i].match(/.*4.*/) != null;
+const isNotImageFour = (_, i) : boolean => !isImageFour(_, i);
+
+
+const trainImg : ImageDescription[] = images.filter(isNotImageFour);
+const testImg : ImageDescription[] = images.filter(isImageFour);
+
+console.log("testImages: "+testImg.map(id => id.name));
+console.log("trainImg: "+trainImg.map(id => id.name));
+
