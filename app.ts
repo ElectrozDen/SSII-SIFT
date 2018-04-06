83bdb39265d3fc4c7f48ea00ddff03c572239e13
Corentin Artaud <corentin.artaud@etu.unice.fr>
2018-04-06 22:52:00 +0200

    changing dependencie for kmeans and removing logos from images

--- app.ts
+++ app.ts
@@ -1,32 +1,42 @@
 import * as cv from 'opencv4nodejs';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as kmeans from 'ml-kmeans'
+import { SIFTDetector, KeyPoint, Mat } from 'opencv4nodejs';
 
 class ImageDescription {
+    private static detector :SIFTDetector = new SIFTDetector();
+
     name : string;
-    image : cv.Mat;
+    image : Mat;
     label : number;
-    constructor(name: string, image: cv.Mat){
+    keypoints: KeyPoint[];
+    descriptors: Mat;
+    constructor(name: string, image: Mat){
         this.name = name;
         this.image = image;
         this.label = -1;
+        this.keypoints = ImageDescription.detector.detect(this.image);
+        this.descriptors = ImageDescription.detector.compute(this.image, this.keypoints);
+    }
+
+    drawWithKeypoints() : void {
+        const im : Mat = cv.drawKeyPoints(this.image, this.keypoints);
+        cv.imwriteAsync("./bin/keyPoints_"+this.name, im);
     }
 }
 
 const dirPath : string = path.resolve('./images');
 const files : string [] = fs.readdirSync(dirPath);
+const isImageFour = (_, i : number) : boolean => files[i].match(/.*4.*/) != null;
+const isNotImageFour = (_, i) : boolean => !isImageFour(_, i);
 
 const images : ImageDescription[] = files
     .map(file => path.resolve(dirPath,file))
     .map(filepath => cv.imread(filepath))
     .map(img => img.bgrToGray())
     .map((realImg, i) => new ImageDescription(files[i], realImg));
-
-
-
-const isImageFour = (_, i : number) : boolean => files[i].match(/.*4.*/) != null;
-const isNotImageFour = (_, i) : boolean => !isImageFour(_, i);
+images.map(im => console.log('name: '+im.name+' number of keypoints '+im.keypoints.length))
+images.map(im => im.drawWithKeypoints());
 
 
 const trainImg : ImageDescription[] = images.filter(isNotImageFour);
@@ -34,4 +44,6 @@ const testImg : ImageDescription[] = images.filter(isImageFour);
 
 console.log("testImages: "+testImg.map(id => id.name));
 console.log("trainImg: "+trainImg.map(id => id.name));
+
+
 
